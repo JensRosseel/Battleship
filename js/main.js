3dc92ed3c9eac4ad3798518e3a73f87a532f894c
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-04-17 22:50:34 +0200

    Add battleships with drag and drop, rotation functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const width = 10
     const userGrid = document.querySelector('.user-grid')
     const opponentGrid = document.querySelector('.opponent-grid')
     const userSquares = []
     const opponentSquares = []
+    const shipsDisplay = document.querySelector('.ships-display')
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
+    const rotateButton = document.querySelector('#rotate')
+    let isHorizontal = true
 
     function createBoard(grid, squares) {
         for (let i = 0; i < width*width; i++) {
@@ -12,6 +22,102 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
         }
     }
+    
     createBoard(userGrid, userSquares)
     createBoard(opponentGrid, opponentSquares)
+
+    ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+    userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
+    userSquares.forEach(square => square.addEventListener('dragover', dragOver))
+    userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    userSquares.forEach(square => square.addEventListener('drop', dragDrop))
+    userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
+  
+    let selectedShipNameWithIndex
+    let draggedShip
+    let draggedShipLength
+  
+    ships.forEach(ship => ship.addEventListener('mousedown', (e) => {
+      selectedShipNameWithIndex = e.target.id
+      console.log(selectedShipNameWithIndex)
+    }))
+  
+    function dragStart() {
+      draggedShip = this
+      draggedShipLength = this.childNodes.length
+      console.log(draggedShip)
+    }
+  
+    function dragOver(e) {
+      e.preventDefault()
+    }
+  
+    function dragEnter(e) {
+      e.preventDefault()
+    }
+  
+    function dragLeave() {
+      console.log('drag leave')
+    }
+  
+    function dragDrop() {
+      let shipNameWithLastId = draggedShip.lastChild.id
+      let shipClass = shipNameWithLastId.slice(0, -2)
+      console.log(shipClass)
+      let lastShipIndex = parseInt(shipNameWithLastId.substr(-1))
+      let shipLastId = lastShipIndex + parseInt(this.dataset.id)
+      console.log(shipLastId)
+      const notAllowedHorizontal = [0,10,20,30,40,50,60,70,80,90,1,11,21,31,41,51,61,71,81,91,2,22,32,42,52,62,72,82,92,3,13,23,33,43,53,63,73,83,93]
+      const notAllowedVertical = [99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60]
+      
+      let newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastShipIndex)
+      let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex)
+  
+      selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1))
+  
+      shipLastId = shipLastId - selectedShipIndex
+      console.log(shipLastId)
+  
+      if (isHorizontal && !newNotAllowedHorizontal.includes(shipLastId)) {
+        for (let i=0; i < draggedShipLength; i++) {
+          userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
+        }
+      } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId)) {
+        for (let i=0; i < draggedShipLength; i++) {
+          userSquares[parseInt(this.dataset.id) - selectedShipIndex + width*i].classList.add('taken', shipClass)
+        }
+      } else return
+  
+      shipsDisplay.removeChild(draggedShip)
+    }
+  
+    function dragEnd() {
+      console.log('dragend')
+    }
+
+    function rotate() {
+        if (isHorizontal) {
+            destroyer.classList.toggle('destroyer-container-vertical')
+            submarine.classList.toggle('submarine-container-vertical')
+            cruiser.classList.toggle('cruiser-container-vertical')
+            battleship.classList.toggle('battleship-container-vertical')
+            carrier.classList.toggle('carrier-container-vertical')
+            isHorizontal = false
+            console.log(isHorizontal)
+            return
+        }
+        if (!isHorizontal) {
+            destroyer.classList.toggle('destroyer-container-vertical')
+            submarine.classList.toggle('submarine-container-vertical')
+            cruiser.classList.toggle('cruiser-container-vertical')
+            battleship.classList.toggle('battleship-container-vertical')
+            carrier.classList.toggle('carrier-container-vertical')
+            isHorizontal = true
+            console.log(isHorizontal)
+            return
+        }
+    }
+    rotateButton.addEventListener('click', rotate)
+  
 })
