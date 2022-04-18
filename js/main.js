cccf7619a0bbb0af12d86fe932fe4f5be119619b
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-04-18 16:54:18 +0200

    Add cpu ships randomly

--- js/main.js
+++ js/main.js
@@ -12,7 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const battleship = document.querySelector('.battleship-container')
     const carrier = document.querySelector('.carrier-container')
     const rotateButton = document.querySelector('#rotate')
+    const turnDisplay = document.querySelector('#turn')
+    const infoDisplay = document.querySelector('#info')
     let isHorizontal = true
+    let isGameOver = false
+    let currentPlayer = 'user'
 
     function createBoard(grid, squares) {
         for (let i = 0; i < width*width; i++) {
@@ -119,5 +123,64 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     rotateButton.addEventListener('click', rotate)
+
+    const shipArray = [
+      {
+        name: 'destroyer',
+        directions: [
+          [0, 1],
+          [0, width]
+        ]
+      },
+      {
+        name: 'submarine',
+        directions: [
+          [0, 1, 2],
+          [0, width, width*2]
+        ]
+      },
+      {
+        name: 'cruiser',
+        directions: [
+          [0, 1, 2],
+          [0, width, width*2]
+        ]
+      },
+      {
+        name: 'battleship',
+        directions: [
+          [0, 1, 2, 3],
+          [0, width, width*2, width*3]
+        ]
+      },
+      {
+        name: 'carrier',
+        directions: [
+          [0, 1, 2, 3, 4],
+          [0, width, width*2, width*3, width*4]
+        ]
+      },
+    ]
+
+    function generate(ship) {
+      let randomDirection = Math.floor(Math.random() * ship.directions.length)
+      let current = ship.directions[randomDirection]
+      if (randomDirection === 0) direction = 1
+      if (randomDirection === 1) direction = 10
+      let randomStart = Math.abs(Math.floor(Math.random() * opponentSquares.length - (ship.directions[0].length * direction)))
+  
+      const isTaken = current.some(index => opponentSquares[randomStart + index].classList.contains('taken'))
+      const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
+      const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+  
+      if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => opponentSquares[randomStart + index].classList.add('taken', ship.name))
+  
+      else generate(ship)
+    }
+    generate(shipArray[0])
+    generate(shipArray[1])
+    generate(shipArray[2])
+    generate(shipArray[3])
+    generate(shipArray[4])
   
 })
